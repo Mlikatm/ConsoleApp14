4c5c82a516e968418870e08533f1545126a05f21
melika <melikatorabi031@gmail.com>
2023-04-15 23:42:32 +0430

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            int X=Convert.ToInt32(Console.ReadLine());
+            int Y=Convert.ToInt32(Console.ReadLine());
+            double show= Calc(X, Y);
+            if (show>=10)
+            {
+                Console.WriteLine("passed");
+            }
+            else
+            {
+                Console.WriteLine("failed");
+            }
+        }
+        private static double Calc (int X, int Y)
+        {
+            double result = X * 0.35 + Y * 0.65;
+            return result;
+        }
+    }
+}
